f2be5e4852cb7012b5a0c25e2177a4179013a249
zorifudin <zorifimaduddin@gmail.com>
2024-12-09 11:04:47 +0700

    feat: login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { registerService } from "../services/auth/register.service";
+import { loginService } from "../services/auth/login.service";
 
 export const registerController = async (
   req: Request,
@@ -8,6 +9,19 @@ export const registerController = async (
 ) => {
   try {
     const result = await registerService(req.body);
+    res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const loginController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const result = await loginService(req.body);
     res.status(200).send(result);
   } catch (error) {
     next(error);
---
+++ src/services/auth/login.service.ts
@@ -0,0 +1,35 @@
+import { User } from "@prisma/client";
+import { prisma } from "../../lib/prisma";
+import { comparePassword } from "../../lib/argon";
+import { sign } from "jsonwebtoken";
+import { JWT_SECRET } from "../../config";
+
+interface Body extends Pick<User, "email" | "password"> {}
+
+export const loginService = async (body: Body) => {
+  try {
+    const { email, password } = body;
+
+    const user = await prisma.user.findFirst({
+      where: { email: email },
+    });
+
+    if (!user) {
+      throw new Error("Invalid email address!");
+    }
+
+    const isPasswordValid = await comparePassword(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new Error("Invalid password!");
+    }
+
+    const { password: pass, ...userWithoutPassword } = user;
+
+    const token = sign({ id: user.id }, JWT_SECRET!, { expiresIn: "2h" });
+
+    return { ...userWithoutPassword, token };
+  } catch (error) {
+    throw error;
+  }
+};

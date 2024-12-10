f7e9b924f107945fee796953dedd266785545245
zorifudin <zorifimaduddin@gmail.com>
2024-12-10 11:05:48 +0700

    feat: reset password

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { registerService } from "../services/auth/register.service";
 import { loginService } from "../services/auth/login.service";
 import { forgotPasswordService } from "../services/auth/forgot-password.service";
+import { resetPasswordService } from "../services/auth/reset-password.service";
 
 export const registerController = async (
   req: Request,
@@ -36,6 +37,20 @@ export const forgotPasswordController = async (
 ) => {
   try {
     const result = await forgotPasswordService(req.body);
+    res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const resetPasswordController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId = Number(res.locals.user.id);
+    const result = await resetPasswordService(userId, req.body.password);
     res.status(200).send(result);
   } catch (error) {
     next(error);

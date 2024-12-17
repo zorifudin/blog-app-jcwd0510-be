d37ecf7364afd5539d37bda2f44e124778a303c8
zorifudin <zorifimaduddin@gmail.com>
2024-12-17 16:00:55 +0700

    fix:deployment

--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -168,7 +168,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
